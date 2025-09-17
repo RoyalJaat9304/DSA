96d52b06673e000fc39894986055054d1df59728
RoyalJaat9304 <tusharmalhan1@gmail.com>
2025-09-17 18:52:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.45%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>& vis , vector<vector<int>>& adj){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it])
-            dfs(it,vis,adj);
+    int findpar(int i,vector<int>& parent){
+        if(parent[i] == i)return i;
+        return parent[i]=findpar(parent[i],parent);
+    }
+    void Union(int u,int v,vector<int>& parent,vector<int>& rank){
+        int pu=findpar(u,parent);
+        int pv=findpar(v,parent);
+        if(pu == pv){
+            return;
+        }
+        if(rank[pu] < rank[pv])
+        parent[pu] = pv;
+        else if(rank[pu] > rank[pv])
+        parent[pv] = pu;
+        else{
+            parent[pu] = pv;
+            rank[pv]++;
         }
     }
+
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size() < n-1)
         return -1;
-
-        vector<vector<int>>adj(n);
-        for(auto it:connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+        vector<int>parent(n),rank(n,0);
+        int e=connections.size();
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+        for(int i=0;i<e;i++){
+            Union(connections[i][0] , connections[i][1] , parent,rank);
         }
-        vector<int>vis(n,0);
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
-                cnt++;
-            }
+            if(findpar(i,parent) == i)
+            cnt++;
         }
         return cnt-1;
     }

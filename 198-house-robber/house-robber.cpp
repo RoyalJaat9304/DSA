bcebbd9c5a30dc0e8eef755b5421d6b9eed074b8
RoyalJaat9304 <tusharmalhan1@gmail.com>
2025-09-21 16:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.72%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,5 @@
 class Solution {
 public:
-    int fun(vector<int>& nums,int i,vector<int>& dp){
-        if( i == 0 || i == 1)
-        return nums[i];
-
-        if(dp[i] != -1)
-        return dp[i];
-
-        int fs=fun(nums,i-2,dp) + nums[i];
-        int ss=INT_MIN;
-        if(i > 2)
-        ss=fun(nums,i-3,dp) + nums[i];
-
-        return dp[i] = max(fs,ss);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);

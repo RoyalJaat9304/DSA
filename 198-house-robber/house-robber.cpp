8958fd6bd19fd96871f72cdd4b9c70eb77c94285
RoyalJaat9304 <tusharmalhan1@gmail.com>
2025-09-21 16:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.72%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -19,7 +19,18 @@ public:
         vector<int>dp(n,-1);
         if(nums.size() == 1)
         return nums[0];
-        
-        return max(fun(nums,n-1,dp),fun(nums,n-2,dp));
+
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+
+        for(int i=2;i<n;i++){
+            int fs=dp[i-2]+nums[i];
+            int ss=INT_MIN;
+            if(i>2)
+            ss=dp[i-3]+nums[i];
+
+            dp[i] = max(fs,ss);
+        }
+        return max(dp[n-1],dp[n-2]);
     }
 };

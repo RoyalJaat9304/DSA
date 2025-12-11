df5bfc7e85fbae7610d20557f372155f3b4627f3
RoyalJaat9304 <tusharmalhan1@gmail.com>
2025-12-11 22:58:48 +0530

    Time: 29 ms (84.08%) | Memory: 146.5 MB (44.63%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -6,11 +6,6 @@ class Solution {
         int n=nums.length-k+1;
         int[] ans = new int[n];
         for(int i=0;i<nums.length;i++){
-            
-            // while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
-            //     dq.pollFirst();
-            // }
-
             while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
                 dq.pollLast();
             }
